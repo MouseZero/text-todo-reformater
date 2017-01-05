74b5311908cfb7056f6f26dfb6b37e7cc22abdc0
mousezero <rm@mousezero.com>
2017-01-04 22:51:26 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const sourcePath = process.argv[2];
+const targetPath = process.argv[3];
+
+console.log("Source Path" + sourcePath);
+console.log("Target Path" + targetPath);
