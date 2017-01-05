dc4da10b1cd6ddc3741b93a90129b1893399bf5b
mousezero <rm@mousezero.com>
2017-01-04 23:21:22 -0800

    all lines now go through a converter function

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const fs = require('fs');
 const sourcePath = process.argv[2];
 const targetPath = process.argv[3];
 
-console.log("Source Path" + sourcePath);
-console.log("Target Path" + targetPath);
+function convertLine(str){
+  return str;
+}
+
+function writeInfoToFile(data){
+  fs.writeFile(targetPath, data, (err) => {
+    const lines = data.split(/\r|\n/g);
+    const htmlLines = lines.map(convertLine);
+    console.log(htmlLines);
+    if(err)console.log(err);
+  });
+}
 
 fs.readFile(sourcePath, 'utf8', (err, data) => {
-  fs.writeFile(targetPath, data, (err) => {
-    if(err)console.log(err);
-  })
+  writeInfoToFile(data);
 });
